9a46b715517cca1d1b15999acebb4c0eb4236ae7
Navya <navyaachanti7@gmail.com>
2025-09-10 22:02:55 +0530

    Day 22 Completed

---
+++ Day22/145. Binary Tree Postorder Traversal.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> res=new LinkedList<>();
+        Stack<TreeNode> stack=new Stack<>();
+        if(root!=null) stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode node=stack.pop();
+            res.addFirst(node.val);
+            if(node.left!=null) stack.push(node.left);
+            if(node.right!=null) stack.push(node.right);
+        }
+        return res;
+    }
+}
