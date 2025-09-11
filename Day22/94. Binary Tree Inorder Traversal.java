666ec0c05b8bbe7246a6362be2573eba2f41eedc
Navyaaa2708 <155165712+Navyaaa2708@users.noreply.github.com>
2025-09-11 21:22:32 +0530

    Update 94. Binary Tree Inorder Traversal.java

--- Day22/94. Binary Tree Inorder Traversal.java
+++ Day22/94. Binary Tree Inorder Traversal.java
@@ -30,3 +30,15 @@ class Solution {
         return res;
     }
 }
+
+// Recursive Solution
+class Solution {
+    List<Integer> res=new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root==null)  return res;
+        inorderTraversal(root.left);
+        res.add(root.val);
+        inorderTraversal(root.right);
+        return res;
+    }
+}
