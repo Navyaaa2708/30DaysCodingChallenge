e046486e6042ab3ae84b2ba0b6bc0a6fe244062e
Navyaaa2708 <155165712+Navyaaa2708@users.noreply.github.com>
2025-09-11 21:23:09 +0530

    Update 144. Binary Tree Preorder Traversal.java

--- Day22/144. Binary Tree Preorder Traversal.java
+++ Day22/144. Binary Tree Preorder Traversal.java
@@ -27,3 +27,17 @@ class Solution {
         return res;
     }
 }
+
+//Recursive Approach
+class Solution {
+    List<Integer> res=new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root==null){
+            return res;
+        }
+        res.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return res; 
+}
+}
