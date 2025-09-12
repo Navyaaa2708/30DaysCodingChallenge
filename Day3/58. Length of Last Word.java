fa34eee59db4376804ab311bb7be0e10e11e7d8a
Navyaaa2708 <155165712+Navyaaa2708@users.noreply.github.com>
2025-09-12 06:06:55 +0530

    Update 58. Length of Last Word.java

--- Day3/58. Length of Last Word.java
+++ Day3/58. Length of Last Word.java
@@ -1,6 +1,26 @@
+//Brute
 class Solution {
     public int lengthOfLastWord(String s) {
         String[] arr = s.split(" ");
         return arr[arr.length-1].length();
+    }
+}
+// TC- O(n)
+// SC - O(n) -> this needs to be eliminated
+
+//optimal 
+// Here we can eliminate that extra space
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int length=0;
+        int i=s.length()-1;
+        while(i>=0 && s.charAt(i)==' '){
+            i--;
+        }
+        while(i>=0 && s.charAt(i)!=' '){
+            length++;
+            i--;
+        }
+        return length;
     }
 }
